a5fc35f34b58ef640f06842e3c6ab16eec5225fd
JorgoQ <47246814+jorgo-q@users.noreply.github.com>
2025-12-04 19:01:28 -0600

    Fix page title formatting in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # ---------- Page config ----------
 st.set_page_config(
-    page_title="HBR Uber Case Study Analysis Dashboard",
+    page_title="HBR - Uber Case Study Analysis Dashboard",
     layout="wide"
 )
 
